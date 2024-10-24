bd5a04db6b9de306d92a09baa6337a1a9d8d4688
dimitrius <dkuleshov0506@gmail.com>
2024-10-24 23:51:32 +0300

    коммит ради коммита

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,6 +5,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Chepushnya')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
