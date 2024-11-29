4c46670f6f3052c83e8f994d0dea765f240c3114
shpolelaote <hivefivers@gmail.com>
2024-11-30 02:10:47 +0300

    Реализация класса Prognosis
    Написал класс Prognosis для формирования JSON прогноза для выбранного продукта (подробнее о классе ниже) на определенный период. Пример инициализации и работы класса представлен внизу скрипта quantity_forecast.py.
    Формат выходного JSON прогноза:
        'date' (строчного формата ггг-мм-01);
        'quantity' (integer);
        'actual' (True - исходные данные таблицы, если на эти даты уже есть фактические данные в датасете, False - спрогнозированные данные).
    
    Класс товаров Product был реализован для тестов. Его следует удалить. Только оповестите меня об этом, чтобы я внес поправки. Можно и без меня, если разобрались с классом.

--- product quantity forecast/quantity_forecast.py
+++ product quantity forecast/quantity_forecast.py
@@ -1,96 +1,138 @@
 import pandas as pd
 from prophet import Prophet
-import matplotlib.pyplot as plt
+import datetime
 
-'''
-    Возвращает датафрейм в формате дата (год-месяц) и число остатков на заданный промжуток.
-    Выполняет прогноз числа остатков, если требуемых дат нет в датасете.
+class Product:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
-    Входные данные:
-    df - датафрейм со столбацами 'date' (datetime формата ггг-мм-01), 'quantity' (integer);
-    start_date - дата с которой требуются данные об остатках;
-    end_date - дата до которой требуются данные об остатках.
+class Prognosis:
+    # template link to datasets for prognosis
+    LOCAL_FILE = r'https://raw.githubusercontent.com/Overtaken5/software-service-project/refs/heads/master/datasets/product_id.csv'
+    '''
+    product - instance of Product class
+    start_date, end_date - string in yyyy-mm-dd format
+    '''
+    def __init__(self, product, start_date, end_date):
+        self.product = product
+        self.start_date = start_date
+        self.end_date = end_date
 
-    Выходные данные:
-    filtered_df - датафрейм со столбацами 'date' (datetime формата ггг-мм-01), 'quantity' (integer).
-'''
-def filter_and_extrapolate(df, start_date, end_date):
-    # Ensure the dates are in datetime format
-    start_date = pd.to_datetime(start_date, format='%Y-%m-%d')
-    end_date = pd.to_datetime(end_date, format='%Y-%m-%d')
+        # dataframe that stores prognosis results
+        self.series_df = None
     
-    # Filter by chosen sub-category
-    filtered_df = df.copy()
-    
-    # Convert 'Order Date' to datetime if it's not already
-    filtered_df['date'] = pd.to_datetime(filtered_df['date'], format='%Y-%m')
+    # get a dataset to train a forecast model
+    def get_train_dataset(self):
+        product_id = self.product.id
+        df = pd.read_csv(Prognosis.LOCAL_FILE.replace('product_id', str(product_id)))
 
-    max_actual_date = max(filtered_df['date'])
+        # prepare for filter_and_extrapolate function (read its comment above)
+        df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+        df['date'] = df['date'].dt.to_period('M').dt.to_timestamp()
+
+        return df
+
+    # returns False if it's impossible to retrieve or forecast 
+    # for the given dates based on train data
+    def is_prognosable(self):
+        if pd.to_datetime(self.start_date, format='%Y-%m-%d').replace(day=1) < self.get_train_dataset()['date'].min():
+            return False
+        else:
+            return True
+
+    # form series of prognose for the given dates
+    def prognose(self):
+        series_train_df = self.get_train_dataset()
+        series_df = Prognosis.filter_and_extrapolate(series_train_df, self.start_date, self.end_date)
+        self.series_df = series_df
     
-    # Filter by date range
-    filtered_df = filtered_df[(filtered_df['date'] >= start_date) & (filtered_df['date'] <= end_date)]
-    
-    # If max date is less than the end date, extrapolate using Prophet
-    if filtered_df.empty or filtered_df['date'].max() < end_date:
-        # Prepare data for Prophet
-        df_prophet = df[['date', 'quantity']].rename(columns={'date': 'ds', 'quantity': 'y'})
+    # return a json object of series
+    # each internal json represents a row in a dataframe
+    def get_json_prognosis(self):
+        # check if it's possible to prognose/retrieve data
+        if self.is_prognosable() == False:
+            min_date = self.get_train_dataset()['date'].min()
+            raise Exception(f'Prognosis date must be at least {min_date}.')
+            return None
         
-        # Initialize and fit Prophet model
-        model = Prophet()
-        model.fit(df_prophet)
+        # set up prognose if not done yet
+        if self.series_df is None:
+            self.prognose()
         
-        # Create future dates up to the end_date
-        max_date = df_prophet['ds'].max()
-        periods = (end_date.year - max_date.year) * 12 + (end_date.month - max_date.month)
-        future = model.make_future_dataframe(periods=periods, freq='M')
+        return self.series_df.to_json(orient='records')
+
+    '''
+        Возвращает датафрейм в формате дата (год-месяц) и число остатков на заданный промжуток.
+        Выполняет прогноз числа остатков, если требуемых дат нет в датасете.
+
+        Входные данные:
+        df - датафрейм со столбацами 'date' (datetime формата ггг-мм-01), 'quantity' (integer);
+        start_date - дата с которой требуются данные об остатках;
+        end_date - дата до которой требуются данные об остатках.
+
+        Выходные данные:
+        filtered_df - датафрейм со столбацами 
+            'date' (строчного формата ггг-мм-01); 
+            'quantity' (integer);
+            'actual' (True - исходные данные таблицы, False - спрогнозированные данные).
+    '''
+    def filter_and_extrapolate(df, start_date, end_date):
+        # Ensure the dates are in datetime format
+        start_date = pd.to_datetime(start_date, format='%Y-%m-%d').replace(day=1)
+        end_date = pd.to_datetime(end_date, format='%Y-%m-%d').replace(day=1)
+
+        filtered_df = df.copy()
+
+        # Convert 'date' to datetime if it's not already
+        filtered_df['date'] = pd.to_datetime(filtered_df['date'], format='%Y-%m')
+
+        max_actual_date = max(filtered_df['date'])
+
+        # Filter by date range
+        filtered_df = filtered_df[(filtered_df['date'] >= start_date) & (filtered_df['date'] <= end_date)]
+
+        # label actual data
+        filtered_df['actual'] = True
+
+        # If max date is less than the end date, extrapolate using Prophet
+        if filtered_df.empty or filtered_df['date'].max() < end_date:
+            # Prepare data for Prophet
+            df_prophet = df[['date', 'quantity']].rename(columns={'date': 'ds', 'quantity': 'y'})
+
+            # Initialize and fit Prophet model
+            model = Prophet()
+            model.fit(df_prophet)
+
+            # Create future dates up to the end_date
+            max_date = df_prophet['ds'].max()
+            periods = (end_date.year - max_date.year) * 12 + (end_date.month - max_date.month) + 1
+            future = model.make_future_dataframe(periods=periods, freq='M')
+
+            # Forecast
+            forecast = model.predict(future)
+
+            # Adjust forecast dates to the first of each month
+            forecast['ds'] = forecast['ds'].apply(lambda x: x.replace(day=1))
+
+            # Append new forecasts to filtered DataFrame
+            forecast_needed = forecast[(forecast['ds'] > max_actual_date.replace(day=1)) & (forecast['ds'] <= end_date)][['ds', 'yhat']]
+            forecast_needed = forecast_needed.rename(columns={'ds': 'date', 'yhat': 'quantity'})
+
+            # label forecasted data
+            forecast_needed['actual'] = False
+
+            # filter data by date
+            filtered_df = pd.concat([filtered_df, forecast_needed], ignore_index=True)
+            filtered_df = filtered_df[(filtered_df['date'] >= start_date) & (filtered_df['date'] <= end_date)]
         
-        # Forecast
-        forecast = model.predict(future)
+        filtered_df['date'] = pd.to_datetime(filtered_df['date'], unit='ms').dt.strftime('%Y-%m-%d')
+        filtered_df['quantity'] = filtered_df['quantity'].round().astype(int)
 
-        # Adjust forecast dates to the first of each month
-        forecast['ds'] = forecast['ds'].apply(lambda x: x.replace(day=1))
-        
-        # Append new forecasts to filtered DataFrame
-        forecast_needed = forecast[(forecast['ds'] > max_actual_date) & (forecast['ds'] <= end_date)][['ds', 'yhat']]
-        forecast_needed = forecast_needed.rename(columns={'ds': 'date', 'yhat': 'quantity'})
-        
-        filtered_df = pd.concat([filtered_df, forecast_needed], ignore_index=True)
-        
-    return filtered_df[['date', 'quantity']]
+        return filtered_df[['date', 'quantity', 'actual']]
 
-'''
-    Строит временной график остатков продукта. 
-    Прогнозированный временной интервал будет прорисован красной линией, а исходные данные - синей.
-    
-    Входные данные:
-    df - датафрейм со столбацами 'date' (datetime формата ггг-мм-01), 'quantity' (integer);
-    product_name - название продукта. На прогноз не влияет. Будет выведен на графике.
-    start_date - дата с которой требуются данные об остатках;
-    end_date - дата до которой требуются данные об остатках.
-
-    Выходные данные:
-    Отсутствуют.
-'''
-def plot_data(df, product_name, start_date, end_date):
-    # Filter and extrapolate data
-    result_df = filter_and_extrapolate(df, start_date, end_date)
-    
-    # Separate actual data and predictions
-    max_actual_date = max(df['date'])
-    actual_data = result_df[result_df['date'] <= max_actual_date]
-    predicted_data = result_df[result_df['date'] >= max_actual_date]
-    
-    # Plotting
-    plt.figure(figsize=(10, 6))
-    plt.plot(actual_data['date'], actual_data['quantity'], label='Actual Data', color='blue')
-    plt.plot(predicted_data['date'], predicted_data['quantity'], label='Predictions', color='red')
-    plt.title(f'Distribution of Quantity for {product_name} Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('Quantity')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-# Example usage:
-
-# plot_data(chairs_df, 'Chairs', '2018-07-01', '2025-07-31')
+product = Product(2, 'name dosn\'t matter')
+start = '2014-01-01'
+end = '2018-06-01'
+prognosis = Prognosis(product, start, end)
+print(prognosis.get_json_prognosis())
