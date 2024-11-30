ee5beabcaed3bebf866ae1b26850bf50d3ce6e08
dimitrius <dkuleshov0506@gmail.com>
2024-12-01 00:23:13 +0300

    изменил архитектуру проекта для наглядности и порядка

---
+++ app/api/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+from app.api.data_from_db import router
+from app.api.login import auth
+
+app = FastAPI()
+app.include_router(router)
+app.include_router(auth)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='127.0.0.1', port=8120)
