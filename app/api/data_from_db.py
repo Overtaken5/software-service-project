e8cd29bc4fe6aa4f73e61068e58f767fd1167264
dimitrius <dkuleshov0506@gmail.com>
2024-12-20 14:37:25 +0300

    добавил графики

--- app/api/data_from_db.py
+++ app/api/data_from_db.py
@@ -1,9 +1,13 @@
+import base64
 import json
+import os
 
+import pandas as pd
 from dateutil.relativedelta import relativedelta
 from fastapi import Form, Request, APIRouter, HTTPException
 from fastapi.params import Depends
-
+from fastapi.responses import FileResponse, JSONResponse
+from pathlib import Path
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import func
 
@@ -11,8 +15,9 @@ from app.api.models.models import Products, Product
 from app.api.db_connection import get_db
 from app.product_quantity_forecast.quantity_forecast import Prognosis, Tovar
 from datetime import datetime, timedelta
-
+from app.product_quantity_forecast.grafik import plot_prognosis
 router = APIRouter()
+
 
 # параметры продукта по названию
 @router.get("/one_product_amount")
@@ -39,7 +44,6 @@ async def get_product_details(product_name: str = Form(...), db: Session = Depen
     }
 
 
-
 # json всех продуктов в БД
 @router.get("/all_products")
 async def get_all_products(db: Session = Depends(get_db)):
@@ -52,12 +56,13 @@ async def get_all_products(db: Session = Depends(get_db)):
         return {"error": "No products found in the database."}
     return product_list
 
-# прогноз количества товара
+
+# прогноз количества товара(в формате название, период прогноза, конечная дата прогноза, количество на конечную дату)
 @router.post("/product_forecast")
 async def get_product_forecast(
-    product_name: str = Form(...),
-    months_ahead: int = Form(...),
-    db: Session = Depends(get_db)
+        product_name: str = Form(...),
+        months_ahead: int = Form(...),
+        db: Session = Depends(get_db)
 ):
     product = db.query(Products).filter(
         (product_name == Products.Name) | (func.upper(Products.Name) == product_name.upper())
@@ -89,3 +94,43 @@ async def get_product_forecast(
         "product_name": product.Name,
         "forecast": [result_forecast]
     }
+
+# выдача графика прогноза
+@router.post("/product_forecast_graph")
+async def get_product_forecast_graph(
+        product_name: str = Form(...),
+        months_ahead: int = Form(...),
+        db: Session = Depends(get_db)
+):
+    product = db.query(Products).filter(
+        (product_name == Products.Name) | (func.upper(Products.Name) == product_name.upper())
+    ).first()
+
+    if product is None:
+        raise HTTPException(status_code=404, detail=f"Tovar '{product_name}' not found.")
+    product_records = db.query(Product).filter(Product.ProductId == product.Id).all()
+    if not product_records:
+        raise HTTPException(status_code=404, detail=f"No records found for product '{product_name}'.")
+
+    current_date = datetime.now()
+    start_date = current_date.strftime("%Y-%m-%d")
+    end_date = (current_date + relativedelta(months=months_ahead)).strftime("%Y-%m-%d")
+    product_instance = Tovar(product.Id, product.Name)
+    prognosis = Prognosis(product_instance, start_date, end_date)
+
+    try:
+        # Получение прогноза
+        forecast = prognosis.get_json_prognosis()
+        forecast_data = json.loads(forecast)  # Парсим строку JSON в список словарей
+
+        # Построение графика
+        graph_filename = plot_prognosis(forecast, start_date, end_date)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error generating forecast or graph: {str(e)}")
+
+        # Возврат файла графика
+    return FileResponse(
+        path=graph_filename,
+        media_type="image/png",
+        filename=graph_filename.split("/")[-1]
+    )
